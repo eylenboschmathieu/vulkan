8ba33ee1524f9715afffd942606284baabb98ea5
Mathieu <mathieu.eylenbosch@gmail.com>
2026-04-26 12:26:06 +0200

    Added context struct

---
+++ blitz/src/context.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code, unsafe_op_in_unsafe_fn, unused_variables, clippy::too_many_arguments, clippy::unnecessary_wraps)]
+
+use log::*;
+use anyhow::{anyhow, Result};
+use vulkanalia::{
+    Entry, loader::{LIBRARY, LibloadingLoader},
+};
+use winit::window::Window;
+
+use crate::{
+    commands::CommandManager, device::Device, instance::Instance, queues::QueueManager, transfer_manager::TransferManager
+};
+
+#[derive(Debug)]
+pub struct Context {
+    entry: Entry,
+    pub instance: Instance,
+    pub device: Device,
+    pub queue_manager: QueueManager,
+    pub command_manager: CommandManager,
+    pub transfer_manager: TransferManager,
+}
+
+impl Context {
+    pub unsafe fn new(window: &Window) -> Result<Self> {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{b}"))?;
+
+        let instance = Instance::new(window, &entry)?;
+        let device = Device::new(&entry, window, &instance)?;
+        let queue_manager = QueueManager::new(&device)?;
+        let command_manager = CommandManager::new(&instance, &device)?;
+        let transfer_manager = TransferManager::new(&device)?;
+
+        Ok(Self { entry, instance, device, queue_manager, command_manager, transfer_manager })
+    }
+
+    pub unsafe fn destroy(&mut self) {
+        self.command_manager.destroy(&self.device);
+        self.transfer_manager.destroy(&self.device);
+        self.device.destroy();
+        self.instance.destroy();
+        info!("~ Context");
+    }
+}
